ec51a98025e138946f31a3d11a17d281b5b12a59
unknown <jonny47169@gmail.com>
2025-10-26 18:51:32 -0500

    second commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,15 @@
-require('dotenv').config();
 module.exports = {
   apps: [{
     name: "api-express-tienda",
     script: "src/index.js",
      env: {
-      ...process.env,  // Copia TODAS las variables del .env
       NODE_ENV: "production",
+      PORT: process.env.PORT,
+      DB_HOST: process.env.DB_HOST,
+      DB_PORT: process.env.DB_PORT,
+      DB_NAME: process.env.DB_NAME,
+      DB_USER: process.env.DB_USER,
+      DB_PASSWORD: process.env.DB_PASSWORD,
     },
      instances: 1,
      exec_mode: "fork",
