fbad0c0bb6459227216888dc9e7d56c853ecfad1
unknown <jonny47169@gmail.com>
2025-10-26 18:30:22 -0500

    second commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,12 +2,8 @@ module.exports = {
   apps: [{
     name: "api-express-tienda",
     script: "src/index.js",
-    node_args: "--env-file=.env",
-    instances: 2,
-    exec_mode: "cluster",
-    env: {
-      NODE_ENV: "production",
-    },
+     instances: 1,
+     exec_mode: "fork",
     error_file: "logs/err.log",
     out_file: "logs/out.log",
     log_date_format: "YYYY-MM-DD HH:mm:ss Z",
