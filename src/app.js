44325d956f1831513d1fc7a8d4b6783ca0e5b3f8
unknown <jonny47169@gmail.com>
2025-10-31 21:07:52 -0500

    crud alumno

--- src/app.js
+++ src/app.js
@@ -12,7 +12,9 @@ import { swaggerSpec } from "./swagger.js";
 const app = express();
 
 app.use(morgan("dev"));
-app.use(helmet());
+app.use(helmet({
+  crossOriginResourcePolicy: { policy: "cross-origin" }, // Permitir recursos cross-origin
+}));
 app.use(cors());
 app.use(express.json({ limit: "50mb" })); // Aumentar límite para imágenes
 app.use(express.urlencoded({ extended: true, limit: "50mb" })); // Para form-data
